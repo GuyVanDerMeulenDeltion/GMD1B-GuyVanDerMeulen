289e0bb6469c4ca99d8fb361e95005e329a8bf20
GuyVanDerMeulenDeltion <guy_meulen@live.nl>
2017-11-05 23:09:14 +0100

    Redid the Scene, Old One Corrupted, Had to rush some things due to Time Shortage.......

---
+++ P5/Unity_Project_Rehash/Low_Poly_3D_Mine/Assets/Scripts/Drill.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drill : MonoBehaviour {
+
+    [Header("Drill Rotation:")]
+    [Range(0, 100)]
+    public float drillRotationSpeed;
+	
+	void Update () {
+        DrillRotate(drillRotationSpeed);
+	}
+
+    //Makes the drill rotate...
+    void DrillRotate(float rotation) {
+        transform.Rotate(rotation, 0, 0);
+    }
+}
---
+++ P5/Unity_Project_Rehash/Low_Poly_3D_Mine/Assets/Scripts/LightFlickering.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightFlickering : MonoBehaviour {
+
+    [Header("Flicker Options:")]
+    [Range(0, 100)]
+    public float flickerTimer;
+    public int onOff;
+
+    public void Start() {
+        StartCoroutine(Flicker(flickerTimer));
+    }
+
+    public void Update() {
+        onOff = Random.Range(0, 2);
+    }
+
+    public IEnumerator Flicker(float time) {
+        yield return new WaitForSeconds(time);
+        if(onOff == 0) {
+            gameObject.GetComponent<Light>().enabled = false;
+        } else {
+            gameObject.GetComponent<Light>().enabled = true;
+        }
+        StartCoroutine(Flicker(flickerTimer));
+    }
+}
