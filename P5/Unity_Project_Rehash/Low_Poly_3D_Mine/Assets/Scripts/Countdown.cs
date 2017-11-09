678a87202a1eed196c581ebd29b9df30b8ba781a
GuyVanDerMeulenDeltion <guy_meulen@live.nl>
2017-11-09 11:37:14 +0100

    Added Movie and Pictures

---
+++ P5/Unity_Project_Rehash/Low_Poly_3D_Mine/Assets/Scripts/Countdown.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Countdown : MonoBehaviour {
+
+    public float seconds = 30;
+	
+	void Update () {
+        seconds -= Time.deltaTime;
+        if(seconds == 0 || seconds < 0) {
+            print("30 seconds passed...");
+        }	
+	}
+}
